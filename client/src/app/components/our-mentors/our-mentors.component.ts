922189a78c694088d551eb6eaf7d06660948fdc4
Yael Avioz <avioz.yael@gmail.com>
2023-03-20 14:51:37 +0200

    add effects files

---
+++ client/src/app/components/our-mentors/our-mentors.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-our-mentors',
+  templateUrl: './our-mentors.component.html',
+  styleUrls: ['./our-mentors.component.scss']
+})
+export class OurMentorsComponent {
+
+}

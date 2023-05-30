db38e37046972c0af794cd4dfdfad218a7abaf34
Yael Avioz <avioz.yael@gmail.com>
2023-05-30 12:42:35 +0300

    add logout

--- client/src/app/components/nav-bar/nav-bar.component.ts
+++ client/src/app/components/nav-bar/nav-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AppComponent } from 'src/app/app.component';
 import { SessionService } from 'src/app/service/sessionService';
 import { User } from 'src/interfaces/user';
 
@@ -11,9 +12,15 @@ export class NavbarComponent {
   isSignInVisible : boolean = false;
   user !: User;
 
-  constructor(private sessionService: SessionService) { }
+  constructor(private sessionService: SessionService, private appComponentParent:AppComponent,) { }
 
   ngOnInit(){
     this.user = this.sessionService.getUserFromSession();
+  }
+
+  logout(){
+    this.sessionService.logout();
+    this.user = this.sessionService.getUserFromSession();
+    this.appComponentParent.user = this.user;
   }
 }

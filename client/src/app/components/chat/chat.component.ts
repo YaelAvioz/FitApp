4c441633244046f9d39e710e789a404793f7ccd6
Yael Avioz <avioz.yael@gmail.com>
2023-06-17 10:31:12 +0300

    chat works

--- client/src/app/components/chat/chat.component.ts
+++ client/src/app/components/chat/chat.component.ts
@@ -1,6 +1,84 @@
+// import { Component } from '@angular/core';
+// import { Store } from '@ngrx/store';
+// import { Observable, take } from 'rxjs';
+// import { SessionService } from 'src/app/service/sessionService';
+// import { sendMessage } from 'src/app/store/chat/chatAction';
+// import { ChatState } from 'src/app/store/chat/chatReducer';
+// import { User } from 'src/interfaces/user';
+// import { ChatBtnComponent } from '../chat-btn/chat-btn.component';
+
+// interface Message {
+//   content: string;
+//   isFromUser: boolean;
+// }
+
+// @Component({
+//   selector: 'app-chat',
+//   templateUrl: './chat.component.html',
+//   styleUrls: ['./chat.component.scss']
+// })
+
+// export class ChatComponent {
+//   response$: Observable<string>;
+//   messages: Message[] = [];
+//   response: any;
+//   userInput: string = '';
+//   currentUser !: User;
+//   placeholderText: string = 'Enter text';
+
+
+//   constructor(private parent: ChatBtnComponent, private sessionService: SessionService, private store: Store<{ chatReducer: ChatState }>) {
+//     this.response$ = this.store.select((state) => {
+//       return state.chatReducer.response;
+//     })
+//     this.currentUser = this.sessionService.getUserFromSession();
+//   }
+
+//   async getMentorAnswer(userMessage: string): Promise<string> {
+//     this.store.dispatch(sendMessage({ username: this.currentUser.username, msg: this.userInput }));
+
+//     return new Promise<string>((resolve) => {
+//       const subscription = this.response$.subscribe(response => {
+//         this.response = response;
+//         console.log("response", this.response.message);
+
+//         setTimeout(() => {
+//           resolve(this.response.message);
+//           subscription.unsubscribe();
+//         }, 3000);
+//       });
+//     });
+//   }
+
+//   async sendMessage() {
+//     const userMessage: Message = {
+//       content:this.userInput,
+//       isFromUser: true
+//     };
+
+//     this.messages.push(userMessage);
+
+//     const mentorAnswerContent = await this.getMentorAnswer(this.userInput);
+
+//     if (mentorAnswerContent) {
+//       const mentorAnswer: Message = {
+//         content: mentorAnswerContent,
+//         isFromUser: false
+//       };
+
+//       this.messages.push(mentorAnswer);
+//     }
+//   }
+
+
+//   isChatVisible() {
+//     this.parent.showChat = !this.parent.showChat;
+//   }
+// }
+
 import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { Observable, take } from 'rxjs';
+import { Observable } from 'rxjs';
 import { SessionService } from 'src/app/service/sessionService';
 import { sendMessage } from 'src/app/store/chat/chatAction';
 import { ChatState } from 'src/app/store/chat/chatReducer';
@@ -32,44 +110,33 @@ export class ChatComponent {
       return state.chatReducer.response;
     })
     this.currentUser = this.sessionService.getUserFromSession();
-  }
 
-  async getMentorAnswer(userMessage: string): Promise<string> {
-    this.store.dispatch(sendMessage({ username: this.currentUser.username, msg: this.userInput }));
-
-    return new Promise<string>((resolve) => {
-      const subscription = this.response$.subscribe(response => {
-        this.response = response;
-        console.log("response", this.response.message);
-
-        setTimeout(() => {
-          resolve(this.response.message);
-          subscription.unsubscribe();
-        }, 3000);
-      });
+    // Subscribe to response$ here and handle the response when it arrives
+    this.response$.subscribe(response => {
+      this.response = response;
+      console.log("response", this.response.message);
+      if (this.response.message) {
+        const mentorAnswer: Message = {
+          content: this.response.message,
+          isFromUser: false
+        };
+        this.messages.push(mentorAnswer);
+      }
     });
   }
 
-  async sendMessage() {
+  sendMessage() {
     const userMessage: Message = {
-      content:this.userInput,
+      content: this.userInput,
       isFromUser: true
     };
 
     this.messages.push(userMessage);
 
-    const mentorAnswerContent = await this.getMentorAnswer(this.userInput);
+    this.store.dispatch(sendMessage({ username: this.currentUser.username, msg: this.userInput }));
 
-    if (mentorAnswerContent) {
-      const mentorAnswer: Message = {
-        content: mentorAnswerContent,
-        isFromUser: false
-      };
-
-      this.messages.push(mentorAnswer);
-    }
+    this.userInput = '';
   }
-
 
   isChatVisible() {
     this.parent.showChat = !this.parent.showChat;
